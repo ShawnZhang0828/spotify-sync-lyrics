133fddce8f254676adc6b65612dd628aa078fcb3
ZHANX326 <zhanx326@mcmaster.ca>
2023-04-14 21:39:16 -0400

    get current playing track

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import Header from './components/Header';
+import axios from 'axios';
 
 function App() {
+  const CLIENT_ID = "07f45b95ceac490ba0871336604107e0"
+  const SCOPE = "user-read-currently-playing"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(element => element.startsWith("access_token")).split("=")[1]
+      
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+  }, []);
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchArtist = async (e) => {
+    e.preventDefault();
+    console.log(token);
+    const data = await axios.get("https://api.spotify.com/v1/me/player/currently-playing", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    })
+
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {!token ? 
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+      : <button onClick={logout}>Logout</button>
+      }
+      {token ? 
+        <form onSubmit={searchArtist}>
+          <input type='text' onChange={e => setSearchKey(e.target.value)}></input>
+          <button type='submit'>Search</button>
+        </form>
+        : <h2>please login</h2>
+      }
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = (props) => {
+  return (
+    <h1>Spotify React</h1>
+  )
+}
+
+export default Header
